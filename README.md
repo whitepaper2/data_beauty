e6881d0caa0c57ad0ae07abb49dd1d5cc9c9c797
whitepaper2 <liyuanpeng1235@163.com>
2017-08-25 14:36:41 +0800

    modify print()

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
 # data_beauty:basic data ananlysis used for study,research
-***
 ## Contents
 1. chapter1:introduction
 2. chapter2:univariate analysis-kde/basic numpy
 3. chapter3:bivariate analysis-scatter plot/splines/losses
-***
 ## Development Environment
-* Python3.6
+* Pycharm/python3.6
 * Anaconda3
 * 《Data Analysis with Open Source Tools》
--- ch2_numpy_in_action.py
+++ ch2_numpy_in_action.py
@@ -9,21 +9,15 @@ vec4 = np.zeros(5)
 for i in range(5):
     vec4[i] = i
 # vec5 = np.loadtxt("./dataset/president.xlsx")
-print(vec1)
-print(vec2)
-print(vec3)
-print(vec4)
+print("create array:", vec1, vec2, vec3, vec4, sep="\n")
 # basic operation
 v1 = vec1 + vec2
 vec1 += vec2
 v3 = 2 * vec3
 v4 = vec4 + 3
 v5 = np.sin(vec4)
-print(v1)
-print(vec1)
-print(v3)
-print(v4)
-print(v5.tolist())
+print("array operation:", v1, vec1, v3, v4, v5.tolist())
+
 from numpy import *
 
 
@@ -31,19 +25,14 @@ def kde(z, w, xv):
     return sum(np.exp(-0.5 * ((z - xv) / w) ** 2) / np.sqrt(2 * np.pi * w ** 2))
 
 
-d = loadtxt("./president.txt", usecols=(3,))
+d = loadtxt("./dataset/president.txt", usecols=(3,))
 w = 2.5
 for x in linspace(min(d) - w, min(d) + w, 1000):
     print(x, kde(x, w, d))
 # shape()\reshape()\slice()
 d = np.linspace(0, 11, 12)
 d.shape = (3, 4)
-print(d)
-print(d[0, :])
-print(d[:, 1])
-print(d[0, 1])
-print(d[0:2, 1])
-print(d[0:1, 1:2])
-print(d[:, [2, 0]])
+print("slice:", d, d[0, :], d[:, 1], d[0, 1], d[0:2, 1], d[0:1, 1:2], d[:, [2, 0]])
+
 k = np.array([False, True, True])
 print(d[k, :])
