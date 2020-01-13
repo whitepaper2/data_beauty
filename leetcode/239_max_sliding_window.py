ecaaca19266b4cc4bbd4bd01934a323bb5ffb033
pengyuan.li <pengyuan.li@tongdun.com>
2020-01-13 11:22:24 +0800

    最小堆做滑动窗口

---
+++ leetcode/239_max_sliding_window.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/1/12 下午4:52
+# @Author  : pengyuan.li
+# @Site    : 
+# @File    : 239_max_sliding_window.py
+# @Software: PyCharm
+
+from typing import List
+import heapq
+
+
+def maxSlidingWindow(nums: List[int], k: int) -> List[int]:
+    n = len(nums)
+    out = []
+    for i in range(n - k + 1):
+        window = nums[i:i + k]
+        if len(window) != 0:
+            out.append(max(window))
+    return out
+
+
+def maxSlidingWindow2(nums: List[int], k: int) -> List[int]:
+    """
+    python heapq维护一个最小堆，数据和下标组成元组，遍历数组逐个加入，如果首元素不在窗口内，则删除，重新构造堆。
+    :param nums:
+    :param k:
+    :return:
+    """
+    if not nums: return []
+    if k >= len(nums): return [max(nums)]
+    heap = []
+    for i in range(0, k): heapq.heappush(heap, (-nums[i], i))
+    maxList = [max(nums[0:k])]
+    for i in range(k, len(nums)):
+        heapq.heappush(heap, (-nums[i], i))
+        while heap[0][1] <= i - k: heapq.heappop(heap)
+        maxList.append(-heap[0][0])
+    return maxList
+
+
+if __name__ == "__main__":
+    nums = [1, 3, -1, -3, 5, 3, 6, 7]
+    k = 3
+    print(maxSlidingWindow(nums, k))
+    print(maxSlidingWindow2(nums, k))
