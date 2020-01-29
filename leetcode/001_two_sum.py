5e48079f8081d694d15f53f807f8f304697df329
pengyuan.li <pengyuan.li@tongdun.com>
2020-01-29 14:11:15 +0800

    遍历一次的方法

--- leetcode/001_two_sum.py
+++ leetcode/001_two_sum.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 # -*- coding: utf-8 -*-
 # @Time    : 2018/11/9 下午10:25
 # @Author  : pengyuan.li
@@ -27,6 +28,23 @@ def twoSum(nums, target):
                 return [i, nums_dict.get(v1)]
 
 
+def twoSum2(nums, target):
+    """
+    遍历一次，字典记录不满足约束条件的值(值，索引)
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+    nums_dict = {}
+    for i in range(len(nums)):
+        cur_val = nums[i]
+        others = target - cur_val
+        if nums_dict.get(others) is not None and nums_dict.get(others) != i:
+            return [i, nums_dict.get(others)]
+        else:
+            nums_dict[cur_val] = i
+
+
 if __name__ == '__main__':
     """
     Given nums = [2, 7, 11, 15], target = 9,
@@ -36,3 +54,4 @@ if __name__ == '__main__':
     nums = [2, 7, 11, 15]
     target = 9
     print(twoSum(nums, target))
+    print(twoSum2(nums, target))
