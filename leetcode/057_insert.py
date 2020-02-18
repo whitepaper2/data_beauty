866bbd99fcef223cbb4057660e20e9df2cd4b164
pengyuan.li <pengyuan.li@tongdun.com>
2020-02-18 17:49:02 +0800

    顺序插入并合并数组

--- leetcode/057_insert.py
+++ leetcode/057_insert.py
@@ -9,9 +9,33 @@ from typing import List
 
 
 def insert(intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-    pass
+    """
+    note: 顺序插入，遇到重叠的则合并
+    :param intervals:
+    :param newInterval:
+    :return:
+    """
+    n = len(intervals)
+    cur_i = 0
+    out_stack = []
+    while cur_i < n and intervals[cur_i][1] < newInterval[0]:
+        out_stack.append(intervals[cur_i])
+        cur_i = cur_i + 1
+    while cur_i < n and intervals[cur_i][0] <= newInterval[1]:
+        newInterval[0] = min(intervals[cur_i][0], newInterval[0])
+        newInterval[1] = max(intervals[cur_i][1], newInterval[1])
+        cur_i = cur_i + 1
+    out_stack.append(newInterval)
+    while cur_i < n:
+        out_stack.append(intervals[cur_i])
+        cur_i = cur_i + 1
+    return out_stack
+
 
 # Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
 # Output: [[1,2],[3,10],[12,16]]
-if __name__=="__main__":
+if __name__ == "__main__":
+    intervals = [[1, 2], [3, 5], [6, 7], [8, 10], [12, 16]]
+    newInterval = [4, 8]
+    print(insert(intervals, newInterval))
     pass
