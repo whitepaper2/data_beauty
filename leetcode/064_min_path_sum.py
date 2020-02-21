8d43e21d0f77dabefed64b0009d7a794670d78b8
pengyuan.li <pengyuan.li@tongdun.com>
2020-02-21 17:02:22 +0800

    贪婪、递归、动态规划还是没搞懂区别

--- leetcode/064_min_path_sum.py
+++ leetcode/064_min_path_sum.py
@@ -58,11 +58,76 @@ def minPathSum2(grid: List[List[int]]) -> int:
     return psum[m - 1]
 
 
+def minPathSum3(grid: List[List[int]]) -> int:
+    """
+    pathsum[i][j] = path[i][j] + min(pathsum[i-1][j], pathsum[i][j-1])，递归法
+    :param grid: 路径数据
+    :return: int，最小路径和
+    """
+    if grid is None:
+        return 0
+    m = len(grid)
+    n = len(grid[0])
+    if m == 0 or n == 0:
+        return 0
+
+    def sub_minPathSum(cur_i, cur_j, grid):
+        if cur_i < 0 or cur_j < 0:
+            return 0
+        if cur_i == 0:
+            return grid[cur_i][cur_j] + sub_minPathSum(cur_i, cur_j - 1, grid)
+        if cur_j == 0:
+            return grid[cur_i][cur_j] + sub_minPathSum(cur_i - 1, cur_j, grid)
+        return min(sub_minPathSum(cur_i - 1, cur_j, grid), sub_minPathSum(cur_i, cur_j - 1, grid)) + grid[cur_i][cur_j]
+
+    return sub_minPathSum(m - 1, n - 1, grid)
+
+
+def minPathSum4(grid: List[List[int]]) -> int:
+    """
+    pathsum[i][j] = path[i][j] + min(pathsum[i-1][j], pathsum[i][j-1])，贪婪法，每次寻找当前位置的最小值
+    :param grid: 路径数据
+    :return: int，最小路径和
+    """
+    if grid is None:
+        return 0
+    m = len(grid)
+    n = len(grid[0])
+    if m == 0 or n == 0:
+        return 0
+
+    right = 0
+    bottom = 0
+    out = grid[0][0]
+    while right < n and bottom < m:
+        if right + 1 < n and bottom + 1 < m and grid[bottom][right + 1] > grid[bottom + 1][right]:
+            out = out + grid[bottom + 1][right]
+            bottom = bottom + 1
+            continue
+        if right + 1 < n and bottom + 1 < m and grid[bottom][right + 1] <= grid[bottom + 1][right]:
+            out = out + grid[bottom][right + 1]
+            right = right + 1
+            continue
+        if right + 1 == n:
+            bottom = bottom + 1
+            while bottom < m:
+                out = out + grid[bottom][right]
+                bottom = bottom + 1
+        if bottom + 1 == m:
+            right = right + 1
+            while right < n:
+                out = out + grid[bottom][right]
+                right = right + 1
+    return out
+
+
 if __name__ == "__main__":
     grid = [
         [1, 3, 1],
         [1, 5, 1],
-        [4, 2, 1]
+        [4, 3, 1]
     ]
     print(minPathSum(grid))
     print(minPathSum2(grid))
+    print(minPathSum3(grid))
+    print(minPathSum4(grid))
